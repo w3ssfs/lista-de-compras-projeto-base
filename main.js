f652cea849b5672254fb46f25a698d88253e0301
Wesley <85897421+w3ssfs@users.noreply.github.com>
2024-02-28 21:53:58 -0300

    this dom

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-let listaDeCompras = ["Arroz", "Feijao", "Bolacha"];
+let listaDeItens = [];
 
-const listaDeItem = {
-  item1: "Arroz",
-  item2: "Suco",
-};
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  salvarItem();
+});
+
+function salvarItem() {
+  const comprasItem = itensInput.value;
+  const checarDuplicado = listaDeItens.some(
+    (element) => element.valor.toUpperCase() === comprasItem.toUpperCase()
+  );
+  if (checarDuplicado) {
+    alert("Duplicado");
+  } else {
+    listaDeItens.push({
+      valor: comprasItem,
+    });
+  }
+
+  console.log(listaDeItens);
+}
